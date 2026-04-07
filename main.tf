fe2bedcc5612c70ee764dfe5673610125cbfc861
Ramesh Chandra Majhee <40921575+rameshmajhee@users.noreply.github.com>
2026-04-08 00:26:32 +0530

    upded
    ddsd

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@ resource "aws_instance" "dev" {
     ami = "ami-01b14b7ad41e17ba4"
     instance_type = "t2.micro"
     tags = {
-      Name = "jenkins-terraform-server"
+      Name = "jenkins-terraform-server-rm"
     }
 }
